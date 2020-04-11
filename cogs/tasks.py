046fad4102072a0de3ac9b97ab69a62c3692b751
Sebastiaan de Oude <sebastiaan.deoude@ugent.be>
2020-04-11 13:55:26 +0200

    Fixed the status loop. Still some odd behaviour when reloading tasks.

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -3,9 +3,9 @@ from discord.ext import commands, tasks
 from itertools import cycle
 
 
-class Tasks(commands.Cog):
+class Tasks(commands.Cog): # cog to define tasks.
 
-    def __init__(self, bot):
+    def __init__(self, bot): # make sure to do *function*.start() to start the task.
         self.bot = bot
         self.status = cycle(['Your Dota 2 Replays', 'A Well-Executed Fountain Hook', 'The Latest Patch Updates',
                              'You Lose, Again...', 'Sniper Being... Sniper', "'Supports' Taking Your Last-Hits",
@@ -14,10 +14,14 @@ class Tasks(commands.Cog):
                              'You Not Getting Any Last-Hits', "Naga Siren's Ultimate Doing Absolutely Nothing"])
         self.change_status.start()
 
-    @tasks.loop(seconds=10.0)
+    @tasks.loop(minutes=20.0)
     async def change_status(self):
-        self.bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=next(self.status)))
+        await self.bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=next(self.status)
+                                                                                             , type=discord.ActivityType.watching))
 
+    @change_status.before_loop
+    async def change_status_before(self):
+        await self.bot.wait_until_ready()
 
 def setup(bot):
     bot.add_cog(Tasks(bot))
